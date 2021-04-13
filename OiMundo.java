1898fc1a5c8eedf8776872b82ba1c21b0f4aac24
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-04-13 12:12:33 -0300

    Segundo Commit

--- OiMundo.java
+++ OiMundo.java
@@ -3,6 +3,14 @@ public class OiMundo {
 	 System.out.println("Hello World! - Estou aprendendo Java!!!");
 	 System.out.println("Vamos que vamos... :-)");
 	 
+	 char a = 'b';
+	 char b = 'c';
+	 
+	 System.out.println("O valor de a é: " + a);
+	 System.outr.println("O valor de b é: " + b);
+	 
+	 
+	 
 	 
 	 
 	
--- OperadorAritimetico1.java
+++ OperadorAritimetico1.java
@@ -2,16 +2,16 @@ public class OperadorAritimetico1 {
 
     public static void main(String[] args) {
         int notaAluno1 = 99;
-	int notaAluno2 = 80;
-	int notaAluno3 = 53;
+		int notaAluno2 = 80;
+		int notaAluno3 = 53;
 
-	int totalGeral = notaAluno1 + notaAluno2 + notaAluno3;
+		int totalGeral = notaAluno1 + notaAluno2 + notaAluno3;
 	
-	System.out.println ("Total Geral: " + totalGeral);
+		System.out.println ("Total Geral: " + totalGeral);
 
-	int mediaGeral = (notaAluno1 + notaAluno2 + notaAluno3) / 3;
+		int mediaGeral = (notaAluno1 + notaAluno2 + notaAluno3) / 3;
 
-	System.out.println("A media do Aluno: " + mediaGeral);
+		System.out.println("A media do Aluno: " + mediaGeral);
 
 
 
