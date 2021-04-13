ba6c901000ddee1a77cd497d0f59d4e985d8d81f
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-04-13 13:46:23 -0300

    Terceiro Commit

---
+++ TipoPrimitivo.java
@@ -0,0 +1,29 @@
+public class TipoPrimitivo{
+	public static void main(String[] args){
+		
+		double precoProduto = 9.43;
+		
+		boolean alunoMatriculado = true;
+		boolean clienteBloqueado = false;
+		
+		char turma = 'A';
+		char tipoCliente = '2';
+		char simbolo = '@';
+		
+		long populacaoUberlandia = 650000;
+		
+		System.out.println(populacaoUberlandia);
+		
+		long populacaoMundial = 7000000000l;
+		System.out.println(populacaoMundial);
+		
+		float saldoConta = 1030.43f;
+		System.out.println(saldoConta);
+		
+		
+		
+	}
+	
+	
+	
+}
