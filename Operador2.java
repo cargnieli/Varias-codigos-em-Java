8e73a8db5fcf2d421762b40198cdc7c45eaa6b0b
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-04-14 07:00:02 -0300

    Mais código em Java

---
+++ Operador2.java
@@ -0,0 +1,23 @@
+public class Operador2 {
+	
+	public static void main(String[] args){
+		
+		int total = 10;
+		total += 3;
+		System.out.println("Total: " + total); 
+		total -= 2;
+		System.out.println("Total: " + total);
+		total *= 3;
+		System.out.println("Total: " + total);
+		total /= 3;
+		System.out.println("Total: " + total);
+		total %= 5;
+		System.out.println("Total: " + total);
+		
+		
+		
+	}
+	
+	
+	
+}
