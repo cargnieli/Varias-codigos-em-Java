fe24793da6d80b40692a68ea3066e57e6d5ef1a4
Walter Donizety Cargnieli <52250107+cargnieli@users.noreply.github.com>
2021-04-14 08:26:42 -0300

    Mais

---
+++ ConversaoTipoPrimitivo5.java
@@ -0,0 +1,22 @@
+public class ConversaoTipoPrimitivo5{
+	
+	public static void main(String[] args){
+		
+		float c = 934.5f;
+		
+		double d = c;
+		
+		System.out.println(d);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
---
+++ ConversaoTipoPrimitivo6.java
@@ -0,0 +1,22 @@
+public class ConversaoTipoPrimitivo6{
+	
+	public static void main(String[] args){
+		
+		double largura = 100;
+		
+		int tamanho = (int) largura;
+		
+		
+		System.out.println(tamanho);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
